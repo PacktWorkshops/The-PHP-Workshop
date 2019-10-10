307d3d53c34f7212b33d91ec9983071413a97ff2
rutuja027 <40690593+rutuja027@users.noreply.github.com>
2019-10-10 10:10:22 +0530

    Update AbstractVehicle.php

--- Chapter05/Exercise5.6/AbstractVehicle.php
+++ Chapter05/Exercise5.6/AbstractVehicle.php
@@ -1,5 +1,4 @@
 <?php
-
 abstract class AbstractVehicle
 {
     public $make;
@@ -9,7 +8,6 @@ abstract class AbstractVehicle
     private $engineNumber;
     public static $counter = 0;
     protected $engineStatus = false;
-
     function __construct($make = 'DefaultMake', $model = 'DefaultModel', $color = 'DefaultColor', $wheels = 4, $engineNo = 'XXXXXXXX')
     {
         $this->make = $make;
@@ -19,64 +17,51 @@ abstract class AbstractVehicle
         $this->engineNumber = $engineNo;
         self::$counter++;
     }
-
     function getMake()
     {
         return $this->make;
     }
-
     function getModel()
     {
         return $this->model;
     }
-
     function getColor()
     {
         return $this->color;
     }
-
     function getNoOfWheels()
     {
         return $this->noOfWheels;
     }
-
     function getEngineNumber()
     {
         return $this->engineNumber;
     }
-
     function setMake($make)
     {
         $this->make = $make;
     }
-
     function setModel($model)
     {
         $this->model = $model;
     }
-
     function setColor($color)
     {
         $this->color = $color;
     }
-
     function setNoOfWheels($wheels)
     {
         $this->noOfWheels = $wheels;
     }
-
     function setEngineNumber($engineNo)
     {
         $this->engineNumber = $engineNo;
     }
-
     abstract function start();
-
     function stop()
     {
         $this->engineStatus = false;
     }
-
     function getEngineStatus()
     {
         return $this->engineStatus;
