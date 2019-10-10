6626c40f8a4202e6025eec2f3a9e6146a470557d
Rutuja Yerunkar <rutujay@packt.com>
2019-10-10 11:54:25 +0530

    Update MyMagicClass.php

--- Chapter05/Examples/MyMagicClass.php
+++ Chapter05/Examples/MyMagicClass.php
@@ -1,10 +1,17 @@
 <?php
-class MyMagicClass 
+class MyMagicClass
 {
-    private $arr = array();
+    private $arr = array('dynamicAttribute' => NULL,'anotherAttribute' => NULL);
     public function __set($attribute, $value)
     {
-        $this->arr[$attribute] = $value;
+        if (array_key_exists($attribute, $this->arr))
+        {
+            $this->arr[$attribute] = $value;
+        } 
+        else 
+        {
+            echo 'Error: the attribute is not allowed. ';
+        }
     }
     public function __get($attribute)
     {
@@ -14,10 +21,12 @@ class MyMagicClass
         } 
         else 
         {
-            echo 'Error: undefined attribute.';
+            echo 'Error: undefined attribute. ';
         }
     }
 }
 $object = new MyMagicClass();
 $object->dynamicAttribute = 'I am magic';
-echo $object->dynamicAttribute . PHP_EOL; //outputs, I am magic
+echo $object->dynamicAttribute . PHP_EOL;
+$object->testAttribute = 'test'; //outputs, Error: the attribute is not allowed.
+echo $object->testAttribute . PHP_EOL; //outputs, Error: undefined attribute.
