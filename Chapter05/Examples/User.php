dfc96e3dacb8c2dde284877c748c09dba2098963
rutuja027 <40690593+rutuja027@users.noreply.github.com>
2019-10-10 10:18:23 +0530

    Add files via upload

---
+++ Chapter05/Examples/User.php
@@ -0,0 +1,25 @@
+<?php
+class User 
+{
+    public $name;
+    private $mailer;
+    private $database;
+    
+    function __construct(string $name, Mailer $mailer, Database $db)
+    {
+        $this->name = $name;
+        $this->mailer = $mailer;
+        $this->database = $db;
+    }
+}
+class Mailer
+{
+    //class body
+}
+class Database
+{
+    //class body
+}
+$mailer = new Mailer();
+$database = new Database();
+$user = new User('John Doe', $mailer, $database);
