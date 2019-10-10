343776eea0986ac7090437ff400613ea56985bb3
rutuja027 <40690593+rutuja027@users.noreply.github.com>
2019-10-10 20:26:59 +0530

    Add files via upload

---
+++ Chapter09/Activity9.1/Example.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Packt;
+
+use Monolog\Logger;
+
+Class Example
+{
+	public function __construct(Logger $logger)
+	{
+		$this->logger = $logger;
+	}
+
+	public function doSomething()
+	{
+		echo "Something" . PHP_EOL;
+
+		$this->logger->info('This is an informational message');
+		$this->logger->error('This message logs an error condition');
+		$this->logger->critical('This message logs unexpected exceptions');
+	}
+}
