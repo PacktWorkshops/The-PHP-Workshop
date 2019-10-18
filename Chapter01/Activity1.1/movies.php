a3773f6615161b76f238a34183aa93784b9deb79
rutuja027 <40690593+rutuja027@users.noreply.github.com>
2019-10-18 11:44:10 +0530

    Update movies.php

--- Chapter01/Activity1.1/movies.php
+++ Chapter01/Activity1.1/movies.php
@@ -5,20 +5,21 @@ $year = $_GET['movieYear'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initialscale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title><?php echo $name; ?></title>
-</head>
-<body>
-<div>
-<h1>Information about <?php echo $name; ?></h1>
-<p>
-Based on the input, here is the information so far:
-<br>
-<?php echo $star . ' starred in the movie ' . $name .' which was released in year ' . $year; ?>
-</p>
-</div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title><?php echo $name; ?></title>
+    </head>
+    <body>
+        <div>
+            <h1>Information about <?php  echo $name; ?></h1>
+            <p>
+            Based on the input, here is the information so far:
+            <br>
+            <?php echo $star . ' starred in the movie ' . $name .' 
+                       which was released in year ' . $year; ?>
+            </p>
+        </div>
+    </body>
 </html>
