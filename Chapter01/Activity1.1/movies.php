d2d409fb40e5ab944d92f9dfbc0a0cf866bafa9f
rutuja027 <40690593+rutuja027@users.noreply.github.com>
2019-10-18 11:41:19 +0530

    Update movies.php

--- Chapter01/Activity1.1/movies.php
+++ Chapter01/Activity1.1/movies.php
@@ -7,8 +7,7 @@ $year = $_GET['movieYear'];
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initialscale=
-1.0">
+<meta name="viewport" content="width=device-width, initialscale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title><?php echo $name; ?></title>
 </head>
@@ -18,8 +17,7 @@ $year = $_GET['movieYear'];
 <p>
 Based on the input, here is the information so far:
 <br>
-<?php echo $star . ' starred in the movie ' . $name .
-' which was released in year ' . $year; ?>
+<?php echo $star . ' starred in the movie ' . $name .' which was released in year ' . $year; ?>
 </p>
 </div>
 </body>
