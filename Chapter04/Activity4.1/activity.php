43dc4d60a91028d0dc00ad7cf129010eeb23ab0b
rutuja027 <40690593+rutuja027@users.noreply.github.com>
2019-10-18 12:09:57 +0530

    Update activity.php

--- Chapter04/Activity4.1/activity.php
+++ Chapter04/Activity4.1/activity.php
@@ -58,25 +58,24 @@ return array_sum(func_get_args());
 */
 function performOperation(string $operation)
 {
-switch ($operation) {
-case 'factorial':
-// get the second parameter, it must be ant int, we will cast
-it to int to be sure
-$number = (int) func_get_arg(1);
-return factorial($number);
-case 'sum':
-// get all parameters
-$params = func_get_args();
-// remove the first parameter, because it is the operation
-array_shift($params);
-return call_user_func_array('sum', $params);
-case 'prime':
-$number = (int) func_get_arg(1);
-return prime($number);
-}
+    switch ($operation) {
+        case 'factorial':
+            // get the second parameter, it must be an int.
+            // we will cast it to int to be sure
+            $number = (int) func_get_arg(1);
+            return factorial($number);
+        case 'sum':
+            // get all parameters
+            $params = func_get_args();
+            // remove the first parameter, because it is the operation
+            array_shift($params);
+            return call_user_func_array('sum', $params);
+        case 'prime':
+            $number = (int) func_get_arg(1);
+            return prime($number);
+    }
 }
 
 echo performOperation("factorial", 3) . PHP_EOL;
 echo performOperation('sum', 2, 2, 2) . PHP_EOL;
-echo (performOperation('prime', 3)) ? "The number you entered was prime."
-. PHP_EOL : "The number you entered was not prime." . PHP_EOL;
+echo (performOperation('prime', 3)) ? "The number you entered was prime.". PHP_EOL : "The number you entered was not prime." . PHP_EOL;
